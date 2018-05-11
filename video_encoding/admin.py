261d472052121c2f749e08bd5ab1a9f3a55fc04b
amirrpp <pluton@freeday.com.ua>
2018-05-11 12:16:14 +0300

    added request arg

--- video_encoding/admin.py
+++ video_encoding/admin.py
@@ -10,8 +10,8 @@ class FormatInline(admin.GenericTabularInline):
     extra = 0
     max_num = 0
 
-    def has_add_permission(self):
+    def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, obj=None):
+    def has_delete_permission(self, request, obj=None):
         return False
